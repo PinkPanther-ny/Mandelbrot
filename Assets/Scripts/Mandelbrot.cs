7856f86e842903e03f95d6818eec07c930e36d52
PinkPanther-ny <alvincny529@gmail.com>
2023-01-09 11:04:09 +0800

    Remove unused

--- Assets/Scripts/Mandelbrot.cs
+++ Assets/Scripts/Mandelbrot.cs
@@ -1,21 +1,5 @@
 using UnityEngine;
 
-public static class ExtensionMethod
-{
-    public static Texture2D ToTexture2D(this RenderTexture rTex)
-    {
-        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
-        var oldRT = RenderTexture.active;
-        RenderTexture.active = rTex;
-
-        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
-        tex.Apply();
-
-        RenderTexture.active = oldRT;
-        return tex;
-    }
-}
-
 public class Mandelbrot : MonoBehaviour
 {
     private readonly int _width = Screen.width;
